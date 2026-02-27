0c17c1be0189a3124b27da466ac8b256b8c5be83
capt-anonymous <lalithakash@gmail.com>
2026-02-27 15:58:53 +0530

    your_name Added UsecaseName

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,20 +1,30 @@
 import java.util.Scanner;
+
 public class PalindromeCheckerApp {
-    static void main() {
+
+    public static void main(String[] args) {
+
         boolean isPalindrome = true;
         Scanner sc = new Scanner(System.in);
-        System.out.println("input:\n");
+
+        System.out.println("Input:");
         String name = sc.nextLine();
+
+        String reversed = new StringBuilder(name).reverse().toString();
+
         for (int i = 0; i < name.length(); i++) {
-            if (name.charAt(i) != name.charAt(name.length() - 1 - i)) {
+            if (name.charAt(i) != reversed.charAt(i)) {
                 isPalindrome = false;
                 break;
             }
-            if (isPalindrome) {
-                System.out.println(name + " is a palindrome.");
-            } else {
-                System.out.println(name + " is not a palindrome.");
-            }
         }
+
+        if (isPalindrome) {
+            System.out.println(name + " is a palindrome.");
+        } else {
+            System.out.println(name + " is not a palindrome.");
+        }
+
+
     }
 }
