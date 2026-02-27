5bc13c9694530f2ffcac013a668a058b4b4d161d
Lalith Akash B <lalithakash@gmail.com>
2026-02-27 16:33:58 +0530

    Update PalindromeCheckerApp.java

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,35 +1,29 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        boolean isPalindrome = true;
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Input:");
-        String name = sc.nextLine();
-        char[] chars = name.toCharArray();
-
-        int start = 0;
-        int end = chars.length - 1;
-
-
-        while (start < end) {
-            if (chars[start] != chars[end]) {
+        System.out.print("Enter a word: ");
+        String input = sc.nextLine();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+        boolean isPalindrome = true;
+        for (int i = 0; i < input.length(); i++) {
+            char poppedChar = stack.pop();
+            if (input.charAt(i) != poppedChar) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
-
         if (isPalindrome) {
-            System.out.println(name + " is a palindrome.");
+            System.out.println(input + " is a palindrome.");
         } else {
-            System.out.println(name+ " is not a palindrome.");
+            System.out.println(input + " is not a palindrome.");
         }
-
-
     }
 }
