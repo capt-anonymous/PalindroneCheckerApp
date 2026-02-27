13460a60d40cbad3cf89c62f57217baf907b28f3
capt-anonymous <lalithakash@gmail.com>
2026-02-27 15:34:36 +0530

    your_name Added UsecaseName

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     static void main() {
-        System.out.println("Welocome to palindrome checker app");
+        boolean isPalindrome = true;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("input:\n");
+        String name = sc.nextLine();
+        for (int i = 0; i < name.length(); i++) {
+            if (name.charAt(i) != name.charAt(name.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+            if (isPalindrome) {
+                System.out.println(name + " is a palindrome.");
+            } else {
+                System.out.println(name + " is not a palindrome.");
+            }
+        }
     }
 }
