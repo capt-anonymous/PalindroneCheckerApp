e8c8a37e17ddf43f0e29a9f736127ab82b640462
capt-anonymous <lalithakash@gmail.com>
2026-02-27 16:17:41 +0530

    your_name Added UsecaseName

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,35 +1,30 @@
 import java.util.Scanner;
-
+import java.util.Stack;
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        boolean isPalindrome = true;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Input:");
         String name = sc.nextLine();
-        char[] chars = name.toCharArray();
-
-        int start = 0;
-        int end = chars.length - 1;
-
-
-        while (start < end) {
-            if (chars[start] != chars[end]) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < name.length(); i++) {
+            stack.push(name.charAt(i));
+        }
+        boolean isPalindrome = true;
+        for (int i = 0; i < name.length(); i++) {
+            char poppedChar = stack.pop();
+            if (name.charAt(i) != poppedChar) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
-
         if (isPalindrome) {
             System.out.println(name + " is a palindrome.");
         } else {
-            System.out.println(name+ " is not a palindrome.");
+            System.out.println(name + " is not a palindrome.");
         }
-
 
     }
 }
